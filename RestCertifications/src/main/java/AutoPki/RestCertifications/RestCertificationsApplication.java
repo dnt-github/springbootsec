483989232665b24f471611a00592d4b1636edb3e
dongtruong <dong.truong@gmail.com>
2020-11-08 19:10:31 -0500

    client error 403

--- RestCertifications/src/main/java/AutoPki/RestCertifications/RestCertificationsApplication.java
+++ RestCertifications/src/main/java/AutoPki/RestCertifications/RestCertificationsApplication.java
@@ -2,12 +2,48 @@ package AutoPki.RestCertifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
-public class RestCertificationsApplication{
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class RestCertificationsApplication extends WebSecurityConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestCertificationsApplication.class, args);
 	}
+	
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService());
+    }
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                if (username.equals("Bob")) {
+                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+                throw new UsernameNotFoundException("User not found!");
+            }
+        };
+    }
+	
 		
 }
--- RestCertifications/src/main/java/AutoPki/RestCertifications/controller/UserController.java
+++ RestCertifications/src/main/java/AutoPki/RestCertifications/controller/UserController.java
@@ -2,6 +2,7 @@ package AutoPki.RestCertifications.controller;
 
 import java.security.Principal;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-    @RequestMapping(value = "/user")
+	
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	@RequestMapping(value = "/user")
     public String user(Model model, Principal principal) {
         
         UserDetails currentUser 
