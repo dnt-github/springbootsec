800dcd77b9231becb853a8bd44cf0e811f703588
dongtruong <dong.truong@gmail.com>
2020-11-08 21:28:34 -0500

    success getting subjectdn

--- RestCertifications/src/main/java/AutoPki/RestCertifications/RestCertificationsApplication.java
+++ RestCertifications/src/main/java/AutoPki/RestCertifications/RestCertificationsApplication.java
@@ -1,5 +1,9 @@
 package AutoPki.RestCertifications;
 
+import java.security.cert.X509Certificate;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +12,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -28,8 +35,9 @@ public class RestCertificationsApplication extends WebSecurityConfigurerAdapter{
         http.authorizeRequests().anyRequest().authenticated()
                 .and()
                 .x509()
-                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
-                .userDetailsService(userDetailsService());
+                .x509PrincipalExtractor(extractor());              
+                //.userDetailsService(userDetailsService());
+        	
     }
 	
 	@Bean
@@ -37,13 +45,32 @@ public class RestCertificationsApplication extends WebSecurityConfigurerAdapter{
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                if (username.equals("Bob")) {
+                
+                if (username.contains("BobDole")) {
                     return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
                 }
                 throw new UsernameNotFoundException("User not found!");
             }
         };
     }
+	
+	@Bean 
+	public SubjectDnX509PrincipalExtractor extractor() {
+		return new SubjectDnX509PrincipalExtractor() {
+			@Override 
+			public Object extractPrincipal(X509Certificate cert) {
+				
+				String value = "";
+				if(cert != null) {
+					value = cert.getSubjectDN().getName();
+					System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + value);
+				}
+				return value;
+			}
+			
+		};
+	}
+	
 	
 		
 }
--- RestCertifications/src/main/java/AutoPki/RestCertifications/controller/UserController.java
+++ RestCertifications/src/main/java/AutoPki/RestCertifications/controller/UserController.java
@@ -1,6 +1,9 @@
 package AutoPki.RestCertifications.controller;
 
 import java.security.Principal;
+import java.security.cert.X509Certificate;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -14,8 +17,12 @@ public class UserController {
 	
 	@PreAuthorize("hasAuthority('ROLE_USER')")
 	@RequestMapping(value = "/user")
-    public String user(Model model, Principal principal) {
-        
+    public String user(Model model, Principal principal, HttpServletRequest request) {
+		/*
+		 * X509Certificate[] data = (X509Certificate[])request.getAttribute(
+		 * "javax.servlet.request.X509Certificate"); String value = ""; if(data != null)
+		 * { value = data[0].getSubjectDN().getName(); }
+		 */
         UserDetails currentUser 
           = (UserDetails) ((Authentication) principal).getPrincipal();
         model.addAttribute("username", currentUser.getUsername());
